df6a677b9749fc6627546948072222a5495dcf33
tairoroberto <tairoroberto@hotmail.com>
2019-02-24 13:22:47 -0300

    adicionando exercicios

---
+++ Android/Exerccio1/app/src/androidTest/java/br/com/digitalhouse/ExampleInstrumentedTest.java
@@ -0,0 +1,25 @@
+package br.com.digitalhouse;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        Assert.assertEquals("com.example.exercicio1", appContext.getPackageName());
+    }
+}
