78a3117db25c1c0518c157d53fd6d6dce05328fc
tairoroberto <tairoroberto@hotmail.com>
2019-02-23 22:55:44 -0300

    Adicionando exercicio, validação, ciclo de vida e layouts

---
+++ Android/Exerccio1/app/src/main/java/com/example/exercicio1/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.exercicio1;
+
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText editNome;
+    private TextInputEditText editEmail;
+    private Button btnEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editNome = (TextInputEditText) findViewById(R.id.editNome);
+        editEmail = (TextInputEditText) findViewById(R.id.editEmail);
+        btnEnviar = (Button) findViewById(R.id.btnEnviar);
+
+
+        //  Adicionamos o evento de click no botão
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Verificamos se no nome está vazio, se estiver vazio mostramos a mensagem e coocamos o foco no elemento
+                if (editNome.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, "Preencha o campo nome", Toast.LENGTH_SHORT).show();
+                    editNome.requestFocus();
+                    return;
+                }
+
+                // Verificamos se no email está vazio, se estiver vazio mostramos a mensagem e coocamos o foco no elemento
+                if (editEmail.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, "Preencha o campo e-mail", Toast.LENGTH_SHORT).show();
+                    editEmail.requestFocus();
+                    return;
+                }
+
+                Toast.makeText(MainActivity.this, "Dados OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ Android/ExercicioCiclodevida/app/src/main/java/com/example/exerciciociclodevida/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.exerciciociclodevida;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MEU_LOG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
+}
