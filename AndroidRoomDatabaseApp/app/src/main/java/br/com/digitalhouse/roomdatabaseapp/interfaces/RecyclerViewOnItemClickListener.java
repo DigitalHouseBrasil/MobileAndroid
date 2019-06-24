5310e283d94debd005cda537e5f49a5eddad9ac8
tairoroberto <tairoroberto@hotmail.com>
2019-06-24 19:01:56 -0300

    remove room

--- AndroidRoomDatabaseApp/app/src/main/java/br/com/digitalhouse/roomdatabaseapp/interfaces/RecyclerViewOnItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package br.com.digitalhouse.roomdatabaseapp.interfaces;
-
-import br.com.digitalhouse.roomdatabaseapp.Contato;
-
-public interface RecyclerViewOnItemClickListener {
-    void onItemClick(Contato contato);
-}
