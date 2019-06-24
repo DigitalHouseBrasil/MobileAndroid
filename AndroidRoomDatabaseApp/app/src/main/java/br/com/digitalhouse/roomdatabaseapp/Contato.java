d223928c5c88de4e6459e15ec5f03bc6cfd2d510
tairoroberto <tairoroberto@hotmail.com>
2019-06-23 21:16:39 -0300

    Add room database

---
+++ AndroidRoomDatabaseApp/app/src/main/java/br/com/digitalhouse/roomdatabaseapp/Contato.java
@@ -0,0 +1,61 @@
+package br.com.digitalhouse.roomdatabaseapp;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Contato implements Parcelable {
+    private String nome;
+    private String telefone;
+
+    public Contato() {
+    }
+
+    public Contato(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    protected Contato(Parcel in) {
+        nome = in.readString();
+        telefone = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(nome);
+        dest.writeString(telefone);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Contato> CREATOR = new Creator<Contato>() {
+        @Override
+        public Contato createFromParcel(Parcel in) {
+            return new Contato(in);
+        }
+
+        @Override
+        public Contato[] newArray(int size) {
+            return new Contato[size];
+        }
+    };
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
---
+++ AndroidRoomDatabaseApp/app/src/main/java/br/com/digitalhouse/roomdatabaseapp/interfaces/RecyclerViewOnItemClickListener.java
@@ -0,0 +1,7 @@
+package br.com.digitalhouse.roomdatabaseapp.interfaces;
+
+import br.com.digitalhouse.roomdatabaseapp.Contato;
+
+public interface RecyclerViewOnItemClickListener {
+    void onItemClick(Contato contato);
+}
